94b621e385b5c159abe4ff9371e98441f3bc0fe2
Gal Moore <gal@galmoore.co.uk>
2023-07-06 15:52:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,44 +17,44 @@ os.environ['OPENAI_API_KEY'] = st.secrets["openai_password"]
 # Create instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
 
-# Create and load PDF Loader
-loader = PyPDFLoader("breast-invasive-patient.pdf")
-# loader = PyPDFLoader('/content/drive//My Drive/3_MY_WORK/1_My_projects/3b_langchain_experiment/git_repo/LangchainDocuments/gal.pdf')
-# # # Split pages from pdf 
-# pages = loader.load_and_split()
-document = loader.load() # load sinlge page
-# document
-# Load documents into vector database aka ChromaDB
-store = Chroma.from_documents(document, collection_name='breast_cancer')
-# store
-# Create vectorstore info object - metadata repo?
-vectorstore_info = VectorStoreInfo(
-    name="breast_cancer",
-    description="breast cancer information",
-    vectorstore=store
-)
-# Convert the document store into a langchain toolkit
-toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
-# Add the toolkit to an end-to-end LC
-agent_executor = create_vectorstore_agent(
-    llm=llm,
-    toolkit=toolkit,
-    verbose=True
-)
-st.title('Your journey begins here') # headline of the website
-# Create a text input box for the user
-prompt = st.text_input('respond like an oncologist that specializes in breast cancer - with 30 years experience and IQ 120 and very high emotional intelligence')
+# # Create and load PDF Loader
+# loader = PyPDFLoader("breast-invasive-patient.pdf")
+# # loader = PyPDFLoader('/content/drive//My Drive/3_MY_WORK/1_My_projects/3b_langchain_experiment/git_repo/LangchainDocuments/gal.pdf')
+# # # # Split pages from pdf 
+# # pages = loader.load_and_split()
+# document = loader.load() # load sinlge page
+# # document
+# # Load documents into vector database aka ChromaDB
+# store = Chroma.from_documents(document, collection_name='breast_cancer')
+# # store
+# # Create vectorstore info object - metadata repo?
+# vectorstore_info = VectorStoreInfo(
+#     name="breast_cancer",
+#     description="breast cancer information",
+#     vectorstore=store
+# )
+# # Convert the document store into a langchain toolkit
+# toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+# # Add the toolkit to an end-to-end LC
+# agent_executor = create_vectorstore_agent(
+#     llm=llm,
+#     toolkit=toolkit,
+#     verbose=True
+# )
+# st.title('Your journey begins here') # headline of the website
+# # Create a text input box for the user
+# prompt = st.text_input('respond like an oncologist that specializes in breast cancer - with 30 years experience and IQ 120 and very high emotional intelligence')
 
-# If the user hits enter
-if prompt:
-    # Then pass the prompt to the LLM
-    response = agent_executor.run(prompt)
-    # ...and write it out to the screen
-    st.write(response)
+# # If the user hits enter
+# if prompt:
+#     # Then pass the prompt to the LLM
+#     response = agent_executor.run(prompt)
+#     # ...and write it out to the screen
+#     st.write(response)
 
-    # With a streamlit expander  
-    with st.expander('Document Similarity Search'):
-        # Find the relevant pages
-        search = store.similarity_search_with_score(prompt) 
-        # Write out the first 
-        st.write(search[0][0].page_content) 
+#     # With a streamlit expander  
+#     with st.expander('Document Similarity Search'):
+#         # Find the relevant pages
+#         search = store.similarity_search_with_score(prompt) 
+#         # Write out the first 
+#         st.write(search[0][0].page_content) 
