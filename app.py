ae527a0adedecdb68335d3ceafbd6258023e3eb0
Gal Moore <gal@galmoore.co.uk>
2023-07-06 15:55:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ os.environ['OPENAI_API_KEY'] = st.secrets["openai_password"]
 llm = OpenAI(temperature=0.1, verbose=True)
 
 st.write("yo")
-# # Create and load PDF Loader
-# loader = PyPDFLoader("breast-invasive-patient.pdf")
+# Create and load PDF Loader
+loader = PyPDFLoader("breast-invasive-patient.pdf")
 # # loader = PyPDFLoader('/content/drive//My Drive/3_MY_WORK/1_My_projects/3b_langchain_experiment/git_repo/LangchainDocuments/gal.pdf')
 # # # # Split pages from pdf 
 # # pages = loader.load_and_split()
