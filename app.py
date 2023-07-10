07bbaed6953d72435deef42cb953cd1279f0ecac
Gal Moore <gal@galmoore.co.uk>
2023-07-10 13:20:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ import PyPDF2
 from io import BytesIO
 import openai
 
-# Import vector store stuff
-from langchain.agents.agent_toolkits import (
-    create_vectorstore_agent,
-    VectorStoreToolkit,
-    VectorStoreInfo
-)
+# # Import vector store stuff
+# from langchain.agents.agent_toolkits import (
+#     create_vectorstore_agent,
+#     VectorStoreToolkit,
+#     VectorStoreInfo
+# )
 
 # Set APIkey for OpenAI Service / Can sub this out for other LLM providers
 os.environ['OPENAI_API_KEY'] = st.secrets["openai_password"]
 
-# Create instance of OpenAI LLM
-llm = OpenAI(temperature=0.1, verbose=True)
+# # Create instance of OpenAI LLM
+# llm = OpenAI(temperature=0.1, verbose=True)
 
 # st.write("yo")
 # Create and load PDF Loader
