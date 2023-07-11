7b80cfbabd215ee8c0cfecb8d06184835da7397d
Gal Moore <gal@galmoore.co.uk>
2023-07-11 13:11:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ else:
 
 uploaded_files = st.sidebar.file_uploader("",accept_multiple_files=True, type=['pdf'])
 st.write("uploaded_files",uploaded_files)
-st.write(uploaded_files)
+# st.write(uploaded_files)
 data = []
 filenames = []
 if uploaded_files and st.session_state['the_data_has_been_plotted'] == False:
